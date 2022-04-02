41819aab9a1ce4ad388dbcb6cca1fcfedf2715b3
RAYJBOY <muhammadhamzaimam786@gmail.com>
2022-04-02 13:39:50 +0100

    Added scoring system

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
+//-----------------------------variables---------------------------------------
 var outerCircle = document.querySelector('.external');
 var innerCircle = document.querySelector('.internal');
 var timerBox = document.querySelector('.timer-container');
 var startButton = document.querySelector('.startButton');
 var bodyHTML = document.querySelector('.bodyContainer');
+var score = document.querySelector('.score');
 
 var outerCircleWidth = outerCircle.offsetWidth;
 var outerCircleHeight = outerCircle.offsetHeight;
 var timerHeight = timerBox.offsetHeight;
+var scoreHeight = score.offsetHeight;
+
+var scoreValue = 0;
 
 const gameHTML = `<div class="body-container">
 
@@ -46,44 +51,61 @@ const gameHTML = `<div class="body-container">
     </div><!--end of timer divs -->
   </div><!--end of timer container -->
 
+  <div class="score">
+    <p>score</p>
+  </div>
 
   <div class="external">
     <div class="internal">
     </div>
   </div>
 
-  <script src="./timer.js"></script>
-  <script src="./game.js"></script>
-
 </div><!--end of body-container -->`;
 
 //-----------------------------End of vars-----------------------------------
+
+//-----------------------------Functions-------------------------------------
 const onButtonClick = () => {
-  console.log("in buttonClick")
-  let XCoordinate = Math.floor(Math.random() * window.innerWidth);
-  let YCoordinate = Math.floor(Math.random() * window.innerHeight);
-  if(XCoordinate >= (window.innerWidth-outerCircleWidth)){
-    XCoordinate = (window.innerWidth-outerCircleWidth);
+  if (scoreValue < 20){
+    let XCoordinate = Math.floor(Math.random() * window.innerWidth);
+    let YCoordinate = Math.floor(Math.random() * window.innerHeight);
+    if(XCoordinate >= (window.innerWidth-outerCircleWidth)){
+      XCoordinate = (window.innerWidth-outerCircleWidth);
+    }
+    if(YCoordinate >= (window.innerHeight-outerCircleHeight)){
+      YCoordinate = (window.innerHeight-(outerCircleHeight+timerHeight+scoreHeight));
+    }
+    outerCircle.style.marginTop = YCoordinate.toString()+"px";
+    outerCircle.style.marginLeft = XCoordinate.toString()+"px";
+    scoreValue++;
+    score.innerHTML = scoreValue;
   }
-  if(YCoordinate >= (window.innerHeight-outerCircleHeight)){
-    YCoordinate = (window.innerHeight-(outerCircleHeight+timerHeight));
+
+  else{
+    endGame();
   }
-  outerCircle.style.marginTop = YCoordinate.toString()+"px";
-  outerCircle.style.marginLeft = XCoordinate.toString()+"px";
+
 }
 
 const onStartClick = () => {
   bodyHTML.innerHTML = gameHTML;
   outerCircle = document.querySelector('.external');
+  timerBox = document.querySelector('.timer-container');
+  score = document.querySelector('.score');
+  score.innerHTML = `0`;
   outerCircleWidth = outerCircle.offsetWidth;
   outerCircleHeight = outerCircle.offsetHeight;
-  timerHeight = timerBox.offsetHeight;
   outerCircle.addEventListener('click', onButtonClick);
   const myTimer = new Timer();
   myTimer.startTimer();
 }
 
+function endGame(){
+  console.log("End of game function");
+}
+//---------------------------End of functions----------------------------------
+
+//---------------------------Event listeners-----------------------------------
 outerCircle.addEventListener('click', onButtonClick);
 startButton.addEventListener('click', onStartClick);
-
-//------------------------------------Timer---------------------------------------
+//-----------------------End of event listeners--------------------------------
